1caaf59dcf83c4b4f7fccd9fdc812751206b3119
Lorenzo Luconi Trombacchi <lorenzo.luconi@iit.cnr.it>
2025-07-28 10:57:00 +0200

    Aggiunti un po' di required openapi nelle risorse relative ai Monitor View

--- src/main/java/it/loreluc/sagraservice/monitor/resource/MonitorProductView.java
+++ src/main/java/it/loreluc/sagraservice/monitor/resource/MonitorProductView.java
@@ -1,10 +1,14 @@
 package it.loreluc.sagraservice.monitor.resource;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class MonitorProductView {
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
     private String name;
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
     private Integer initialQuantity;
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
     private Integer availableQuantity;
 }
